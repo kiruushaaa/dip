a5582efc464b6b97667be1bcb4ced6d28f1c45b1
Kirill <kiruxa4866613@gmail.com>
2022-04-29 17:32:02 +0300

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,5 @@
+import styles from './App.module.css';
+
+export const App:React.FC = () => (
+    <div className={ styles.container }>Test app</div>
+);
---
+++ src/utils/dijkstra.js
@@ -0,0 +1,56 @@
+const INITIAL_DATA = {
+    minDistance: Infinity,
+    nearestVertex: null
+};
+
+const findNearestVertex = (distances, visited) => {
+    const { nearestVertex } = Object.keys(distances).reduce(({ minDistance, nearestVertex }, vertex) => {
+        if (!visited[vertex] && (distances[vertex] < minDistance)) {
+            return {
+                minDistance: distances[vertex],
+                nearestVertex: vertex
+            };
+        }
+
+        return { minDistance, nearestVertex };
+    }, INITIAL_DATA);
+
+    return nearestVertex;
+};
+
+const prepareData = (vertices, startVertex) => ({
+    distances: Object.assign({}, ...vertices.map(vertex => ({ [vertex]: Infinity })), { [startVertex]: 0 }),
+    previous: Object.assign({}, ...vertices.map(vertex => ({ [vertex]: null }))),
+    visited: {}
+});
+
+export const dijkstra = (graph, startVertex) => {
+    const { distances, previous, visited } = prepareData(Object.keys(graph), startVertex);
+
+    function handleVertex(vertex) {
+        const activeVertexDistance = distances[vertex];
+
+        const neighbors = graph[activeVertex];
+
+        Object.keys(neighbors).forEach(neighborVertex => {
+            const currentNeighborDistance = distances[neighborVertex];
+            const newNeighborDistance = activeVertexDistance + neighbors[neighborVertex];
+
+            if (newNeighborDistance < currentNeighborDistance) {
+                distances[neighborVertex] = newNeighborDistance;
+                previous[neighborVertex] = vertex;
+            }
+        });
+
+        visited[vertex] = true;
+    }
+
+    let activeVertex = findNearestVertex(distances, visited);
+
+    while(activeVertex) {
+        handleVertex(activeVertex);
+        activeVertex = findNearestVertex(distances, visited);
+    }
+
+    return { distances, previous };
+};
