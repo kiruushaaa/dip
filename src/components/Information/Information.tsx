afbc735d6754b18afbc4194cf82c2fbce05592c9
kiruushaaa <kiruxa4866613@gmail.com>
2022-05-21 23:46:54 +0300

    the most useless part, btw

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -1,5 +1,7 @@
 import cn from 'classnames';
 
+import { ACTIVE_COLOR, VIS_VISITED_NODE_COLOR } from 'helpers/vis';
+
 import global from 'style/global.module.css';
 import styles from './Information.module.css';
 
@@ -7,11 +9,24 @@ export const Information = () => (
     <section className={ styles.wrapper } aria-label="Water">
         <h1 className={ styles.title }><span>Dijkstra</span>&apos;s algorithm. Visualization step by step</h1>
         <p>
-            Чтобы построить граф, используются кнопки
+            Алгоритм <span className={ styles.misc }>Дейкстры</span> находит кратчайшие пути от одной из вершин графа до всех остальных.
+            Алгоритм работает только для графов без рёбер отрицательного.
+        </p>
+        <p>
+            Граф строится при помощи
             <span className={ cn(global.btn, global.btnAppend, styles.btn) }>Append node</span>
             и
-            <span className={ cn(global.btn, global.btnAppend, styles.btn) }>Append edge</span>
-            для добавления вершин и ребер соответственно.
+            <span className={ cn(global.btn, global.btnAppend, styles.btn) }>Append edge</span>,
+             что добавляют вершину и ребро соответственно.
+        </p>
+        <p>
+            <span className={ cn(global.btn, global.btnSuccess, styles.btn) }>Show journey</span>
+            показываеты пошагово, как работает алгоритм, добавляя псевдоребра между вершинами и закрашивая вершины в некоторый цвет
+            (<span style={ { color: ACTIVE_COLOR } }>текущая</span>, <span style={ { color: VIS_VISITED_NODE_COLOR.background } }>посещенная</span>).
+        </p>
+        <p>
+            <span className={ cn(global.btn, global.btnPath, styles.btn) }>Show the shortest path</span>
+            покажет кратчаший путь (если существует) из начальной вершины в выбранную конечную.
         </p>
     </section>
 );
